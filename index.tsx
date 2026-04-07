91a994de37a961dccfe23fe566239a77c93934ed
rslima-lifetime <beto.credito@gmail.com>
2026-04-07 12:11:03 -0300

    feat: enable PWA support with manifest configuration and service worker registration

--- index.tsx
+++ index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
+import { registerSW } from 'virtual:pwa-register';
+
+// Registra o Service Worker (permite a instalação do App)
+registerSW({ immediate: true });
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
